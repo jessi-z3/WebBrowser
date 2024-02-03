661f7ec9882a86eba6a3fd146000f20cf8bf70d7
jessi-z3 <jessi_leeann@yahoo.com>
2024-02-03 14:02:01 -0800

    Initial Commit

---
+++ WebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrowser
+//
+//  Created by Jessi Zimmerman on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
